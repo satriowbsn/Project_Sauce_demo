bf7a9fa0463b80c66d7987d722d0b2fb40e77bea
Satrio Wibisono <satrio.wibisono@mmsgroup.co.id>
2024-10-03 13:35:30 +0700

    initial commit

---
+++ Scripts/Filter_data/Script1727936775423.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.maximizeWindow()
+
+WebUI.navigateToUrl('https://www.saucedemo.com/')
+
+WebUI.setText(findTestObject('Login/Sauce_demo/Username_saucedemo'), 'standard_user')
+
+WebUI.setText(findTestObject('Login/Sauce_demo/Password_saucedemo'), 'secret_sauce')
+
+WebUI.click(findTestObject('Login/Sauce_demo/login_btn'))
+
+WebUI.selectOptionByValue(findTestObject('Sauce_demo/Selection_filter_swag_labs'), 'lohi', false)
+
+WebUI.delay(0)
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.closeBrowser()
+
